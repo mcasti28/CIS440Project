1cdaeb159f723dc3a6d22deb438dc3387852fdbe
Missael <mcasti28@asu.edu>
2019-09-26 16:15:29 -0700

    CIS440

---
+++ ProjectTemplate/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTemplate
+{
+    public class Employee
+    {
+        public int idEmployee;
+        public string userName;
+        public string password;
+        public string firstName;
+        public string lastName;
+        public string phoneNum;
+        public string email;
+    }
+}
