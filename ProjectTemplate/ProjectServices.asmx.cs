03e6accaa11b489b5e7e9b375e9d0f64ef2eb73c
Missael <mcasti28@asu.edu>
2019-10-04 13:42:27 -0700

    updated checkboxes and started asmx function

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -107,5 +107,43 @@ namespace ProjectTemplate
             //return the result!
             return success;
         }
-	}
+
+        [WebMethod(EnableSession = true)]
+        public void Shift(int shift, string date, int idEmployee)
+        {
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["group1"].ConnectionString;
+            //the only thing fancy about this query is SELECT LAST_INSERT_ID() at the end.  All that
+            //does is tell mySql server to return the primary key of the last inserted row.
+            string sqlSelect = "insert into availability (shift, date, idEmployee) " +
+                "values(@shiftValue, @dateValue, @idValue); SELECT LAST_INSERT_ID();";
+
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //sqlCommand.Parameters.AddWithValue("@shiftValue", HttpUtility.UrlDecode(shift));
+            sqlCommand.Parameters.AddWithValue("@dateValue", HttpUtility.UrlDecode(date));
+            //sqlCommand.Parameters.AddWithValue("@idValue", HttpUtility.UrlDecode(idEmployee));
+
+            //this time, we're not using a data adapter to fill a data table.  We're just
+            //opening the connection, telling our command to "executescalar" which says basically
+            //execute the query and just hand me back the number the query returns (the ID, remember?).
+            //don't forget to close the connection!
+            sqlConnection.Open();
+            //we're using a try/catch so that if the query errors out we can handle it gracefully
+            //by closing the connection and moving on
+            try
+            {
+                int accountID = Convert.ToInt32(sqlCommand.ExecuteScalar());
+                //here, you could use this accountID for additional queries regarding
+                //the requested account.  Really this is just an example to show you
+                //a query where you get the primary key of the inserted row back from
+                //the database!
+            }
+            catch (Exception e)
+            {
+            }
+            sqlConnection.Close();
+        }
+    }
+	
 }
